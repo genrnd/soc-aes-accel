7761c9f4e30587fb5b42f0683148fa63c96faa3e
Ivan Oleynikov <io@metrotek.spb.ru>
2017-02-16 14:14:52 +0000

    Set cipher to aes-128-cbc
    git-svn-id: svn://svn.ddg/soc/branches/cpu/aes-crypto-api-iface@71732 b56743da-d8c8-0310-ae72-f310648a94a5

--- tools/openssl_benchmark.c
+++ tools/openssl_benchmark.c
@@ -30,7 +30,7 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
 	 * In this example we are using 256 bit AES (i.e. a 256 bit key). The
 	 * IV size for *most* modes is the same as the block size. For AES this
 	 * is 128 bits */
-	if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+	if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv))
 		handleErrors();
 
 	/* Provide the message to be encrypted, and obtain the encrypted output.
@@ -69,7 +69,7 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
 	 * In this example we are using 256 bit AES (i.e. a 256 bit key). The
 	 * IV size for *most* modes is the same as the block size. For AES this
 	 * is 128 bits */
-	if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+	if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv))
 		handleErrors();
 
 	/* Provide the message to be decrypted, and obtain the plaintext output.
